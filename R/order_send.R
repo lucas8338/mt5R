e27c48f6a2994b551c3decc152e11af8490e3e13
Lucas Monteiro <lucas.ma8338@gmail.com>
2023-02-28 09:13:07 -0300

    1 - fix order_send function 2 - changes version

--- R/order_send.R
+++ R/order_send.R
@@ -4,5 +4,5 @@ order_send<- function(...){
   # not tested
   mt5<- mt5()
   bt<- bt()
-  mt5$order_send(...)[['_asdict']]() %>% reticulate::py_to_r()
+  do.call(mt5$order_send, ...)[['_asdict']]() %>% reticulate::py_to_r()
 }
